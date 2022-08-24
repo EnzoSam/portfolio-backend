253a84a424041ea67fa7731dc27bcfda276fe543
enzo256 <enzosamudio@gmail.com>
2022-08-23 21:30:41 -0300

    Creacion del proyecto

---
+++ src/test/java/com/portfolio/same/portfoliobackend/PortfolioBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.same.portfoliobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
