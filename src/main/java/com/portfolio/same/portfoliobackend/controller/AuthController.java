35e78e512500e5adda75f0cb7085824dd64a7811
enzo256 <enzosamudio@gmail.com>
2022-10-18 23:16:01 -0300

    cors

--- src/main/java/com/portfolio/same/portfoliobackend/controller/AuthController.java
+++ src/main/java/com/portfolio/same/portfoliobackend/controller/AuthController.java
@@ -27,7 +27,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "https://portfolio-fb9ed.firebaseapp.com")
+@CrossOrigin(origins = {"https://portfolio-fb9ed.firebaseapp.com","https://portfolio-fb9ed.web.app"})
 public class AuthController {
 
     @Autowired
--- src/main/java/com/portfolio/same/portfoliobackend/controller/PlaceController.java
+++ src/main/java/com/portfolio/same/portfoliobackend/controller/PlaceController.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "https://portfolio-fb9ed.firebaseapp.com")
+@CrossOrigin(origins = {"https://portfolio-fb9ed.firebaseapp.com","https://portfolio-fb9ed.web.app"})
 public class PlaceController {
    @Autowired
      private IPlaceService service;
