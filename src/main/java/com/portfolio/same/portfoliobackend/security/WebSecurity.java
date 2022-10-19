232ed8c8fb48966b53c1b91a1a55d6311b14ff26
enzo256 <enzosamudio@gmail.com>
2022-10-18 22:22:42 -0300

    cors bean

--- src/main/java/com/portfolio/same/portfoliobackend/security/WebSecurity.java
+++ src/main/java/com/portfolio/same/portfoliobackend/security/WebSecurity.java
@@ -63,6 +63,18 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+http.cors().and().csrf().disable()
+                .authorizeRequests().antMatchers("/api/**").permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests().and().httpBasic();
+                http.headers().cacheControl();        
+        
+                
+                /**
         http.cors().and().csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/api/auth/**").permitAll()
@@ -73,7 +85,7 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
                 .and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         
-        http.addFilterBefore(jwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+        http.addFilterBefore(jwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);**/
     }
 
 }
