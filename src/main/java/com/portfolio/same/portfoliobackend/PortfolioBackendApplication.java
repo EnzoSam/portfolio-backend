40f609b77cb0f8dd9f89be8ce48a1e64343bd119
enzo256 <enzosamudio@gmail.com>
2022-10-18 21:12:36 -0300

    cors bean

--- src/main/java/com/portfolio/same/portfoliobackend/PortfolioBackendApplication.java
+++ src/main/java/com/portfolio/same/portfoliobackend/PortfolioBackendApplication.java
@@ -2,6 +2,9 @@ package com.portfolio.same.portfoliobackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class PortfolioBackendApplication {
@@ -9,5 +12,18 @@ public class PortfolioBackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PortfolioBackendApplication.class, args);
 	}
+        
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+            return new WebMvcConfigurer() {
+                    @Override
+                    public void addCorsMappings(CorsRegistry registry) {
+                            registry.addMapping("/**")
+                                    .allowedOrigins("https://portfolio-fb9ed.web.app")
+                                    .allowedMethods("*")
+                                    .allowedHeaders("*");;
+                    }
+            };
+	}        
 
 }
--- src/main/java/com/portfolio/same/portfoliobackend/controller/PortfolioController.java
+++ src/main/java/com/portfolio/same/portfoliobackend/controller/PortfolioController.java
@@ -15,7 +15,6 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 
-@CrossOrigin(origins = {"*"})
 @RestController
 public class PortfolioController {
     
